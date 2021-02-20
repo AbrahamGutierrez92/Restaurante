4d12e60a212e17a7b1b3ed3a50ef051c14d50d55
AbrahamGutierrez92 <61261412+AbrahamGutierrez92@users.noreply.github.com>
2021-02-19 18:11:58 -0600

    Marketing

---
+++ js/autor.js
@@ -0,0 +1,7 @@
+function Autor(){
+  console.log("Restaurante Bello Amanecer");
+  console.log("Autor: Abraham Gutierrez, Carne: 1511608");
+  console.log("Carrera: Ingeneria en Sistema de informacion");
+  console.log("Modalidad Dominical");
+}
+Autor();
